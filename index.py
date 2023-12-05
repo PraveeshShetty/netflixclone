566628286817508806c7964d547062342dda2676
Praveesh Shetty <shettypraveesh2@gmail.com>
2023-12-05 12:21:29 +0530

    first commit

--- index.py
+++
@@ -1,8 +0,0 @@
-str=input("enter main string:")
-sub=input("enter sub string:")
-try:
-        n=str.find(sub,0,len(str))
-except valueError:
-    print('sub string not found')
-else:
-    print('sub string found at position:',n+1)
--- substring.py
+++
@@ -1,7 +0,0 @@
-str=input("enter main string:")
-sub=input("enter sub string:")
-n=str.find(sub,0,len(str))
-if n==-1:
-    print('sub string not found')
-else:
-    print('sub string found at position:',n+1)
